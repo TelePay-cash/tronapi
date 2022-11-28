4eb8e2d2e1546b65cda4346b752f35fe02586596
hector <hector.alonso@osoigo.com>
2022-11-27 20:18:53 -0500

    remove requirement that we got on eth-account

--- setup.py
+++ setup.py
@@ -77,9 +77,7 @@ install_requires = [
     "toolz>=0.9.0,<1.0.0;implementation_name=='pypy'",
     "cytoolz>=0.9.0,<1.0.0;implementation_name=='cpython'",
 
-    "eth-abi>=2.0.0b6,<3.0.0",
     "git+git://github.com/TelePay-cash/eth_account.git",
-    "eth-utils>=1.3.0,<2.0.0",
     "eth-hash[pycryptodome]>=0.2.0,<1.0.0",
 
     "trx-utils",
